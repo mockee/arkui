222a884028a8fcce579461debb5d54edfe98d40e
mockee <mockee@douban.com>
2012-09-07 00:26:44 +0800

    The concept of a "layout" in Express 3.x had been removed

--- docs/build/index.js
+++ docs/build/index.js
@@ -1,21 +1,20 @@
 var jade = require('jade')
   , fs = require('fs')
-  , tmplDir = __dirname + '/../templates/'
-  , layout, pages;
+  , path = require('path')
+  , pagesPath = __dirname + '/../templates/pages'
+  , pageFile, pageStr, filename
 
-layout = jade.compile(
-  fs.readFileSync(tmplDir + 'layout.jade', 'utf-8'));
 
-fs.readdirSync(tmplDir + '/pages')
+fs.readdirSync(pagesPath)
   .forEach(function(name) {
     if (!name.match(/\.jade$/)) { return; }
-
-    var page = layout({
-      body: jade.compile(fs.readFileSync(
-        tmplDir + 'pages/' + name, 'utf-8'))()
-    }),
+    pageFile = path.join(pagesPath, name);
+    pageStr = jade.compile(
+      fs.readFileSync(pageFile, 'utf-8'), {
+        filename: pageFile
+      })();
 
     filename = name.replace(/jade$/, 'html');
-    fs.writeFileSync(__dirname + '/../' + filename, page, 'utf-8')
+    fs.writeFileSync(__dirname + '/../' + filename, pageStr, 'utf-8')
     console.info('  \033[90m%s \033[37m%s', 'compiled', filename);
   });
